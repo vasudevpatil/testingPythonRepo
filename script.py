3deebeaee1b5e19d8d1e7642f252fd7094853632
vsudevpatil <vsudevpatil@gmail.com>
2019-11-09 01:02:03 +0530

    Removed unwanted code

--- script.py
+++ script.py
@@ -1,24 +1,9 @@
-import sys
-import math
+
 import requests
-import os
 
 
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-print(greet("World"))
-print(greet("Corey"))
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 
-# name = input("Your Name? ")
-# print("Hello ,", name)
